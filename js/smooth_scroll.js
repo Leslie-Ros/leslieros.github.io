0774802e59819d77c9796b457773e41368c36529
Leslie-Ros <aspire-one@hotmail.fr>
2018-04-07 13:50:10 +0200

    real profile + slow scroll

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -1,25 +1,24 @@
 $(document).ready(function(){
 				// NAVIGATION
-				var scroll = 100;
 
 				$('#nav-profil').click(function(){
-					$('html, body').animate({scrollTop : $('#profil').position().top },800);
+					$('html, body').animate({scrollTop : $('#profil').position().top },1000);
 					return false;
 				});
 				$('#nav-formation').click(function(){
-					$('html, body').animate({scrollTop : $('#formation').position().top },800);
+					$('html, body').animate({scrollTop : $('#formation').position().top },1000);
 					return false;
 				});
 				$('#nav-experience').click(function(){
-					$('html, body').animate({scrollTop : $('#experience').position().top },800);
+					$('html, body').animate({scrollTop : $('#experience').position().top },1000);
 					return false;
 				});
 				$('#nav-competences').click(function(){
-					$('html, body').animate({scrollTop : $('#competences').position().top },800);
+					$('html, body').animate({scrollTop : $('#competences').position().top },1000);
 					return false;
 				});
 				$('#nav-contact').click(function(){
-					$('html, body').animate({scrollTop : $('#contact').position().top },800);
+					$('html, body').animate({scrollTop : $('#contact').position().top },1000);
 					return false;
 				});
       })
